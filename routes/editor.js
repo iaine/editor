58c0fefd96836d3c63160321ab35887dd6d45668
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-07-26 10:29:39 +0100

    Removed the duplicate view from home

--- routes/editor.js
+++ routes/editor.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var editor_controller = require('../controllers/editorController');
 
 /* GET users listing. */
-router.get('/', editor_controller.code_view);
+router.get('/', editor_controller.code_edit);
 
 router.get('/about', function(req, res, next) {
   res.send('About editor');
