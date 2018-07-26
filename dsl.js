1d53ff800768e4bc94b4fb18aab8802b57206c33
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-07-26 09:41:11 +0100

    Refactored run into DSL. Removed the if/else loops

--- dsl.js
+++ dsl.js
@@ -4,6 +4,15 @@
 
 var DSL = function () {};
 
+DSL.prototype.runcode = function(codes) {
+    
+    for (let i in codes) {
+        let tmp = codes[i].split(':');
+        console.log('tmp ' + tmp);
+        this[tmp[0]](tmp[1]);
+    }
+}
+
 DSL.prototype.get = function(str) {
   console.log("Arg" + str);
 }
