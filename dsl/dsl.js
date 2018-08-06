f34ec886ef42b480b9988e2ce4ec771680ffaa4a
iaine <iain.emsley@kellogg.ox.ac.uk>
2018-08-06 14:49:27 +0100

    getText option added & data created

--- dsl/dsl.js
+++ dsl/dsl.js
@@ -4,18 +4,34 @@
 
 var DSL = function () {};
 
+DSL.prototype.init = function() {
+  this.data = [];
+}
+
 DSL.prototype.runcode = function(codes) {
     
     for (let i in codes) {
         let tmp = codes[i].split(':');
         console.log('tmp ' + tmp);
-        this[tmp[0]](tmp[1]);
+        let args= tmp[1].substring(1,(tmp[1].length-1)).split(',');
+        this[tmp[0]].apply(this, args);
     }
 }
 
-DSL.prototype.get = function(str) {
-  console.log("Arg" + str);
+DSL.prototype.get = function(ftype, str) {
+  console.log("File " + ftype)
+  switch(ftype) {
+    case 'text':
+    case 'txt':
+      this.data = this.getTxt(str);
+      break;
+    default:
+      console.log('Operation not handled')
+  }
 }
 
+DSL.prototype.getTxt = function(fname) {
+  console.log('in text');
+}
 
 exports.DSL = DSL;
